9297bec20c4edc2261f43214c620c191ea98d79f
AshKetchumPL <kotpolska1123@gmail.com>
2022-04-26 20:32:47 +0200

    ReAdded copyright

--- maincog.py
+++ maincog.py
@@ -1,3 +1,7 @@
+# This bot was proudly coded by >>ash ketchum<<#6595 (https://github.com/AshKetchumPL).
+# Copyright (c) 2022  >>ash ketchum<<#6595 | https://discord.gg/GS3bcWPBuY
+# This bot is under the Mozilla Public License v2 (https://www.mozilla.org/en-US/MPL/2.0/).
+
 # This contains all the commands
 
 import discord, requests, random, time, asyncio, inspect, io, textwrap, traceback, psutil, aiohttp, string, base64
--- selfbot.py
+++ selfbot.py
@@ -1,3 +1,9 @@
+# This bot was proudly coded by >>ash ketchum<<#6595 (https://github.com/AshKetchumPL).
+# Copyright (c) 2022  >>ash ketchum<<#6595 | https://discord.gg/GS3bcWPBuY
+# This bot is under the Mozilla Public License v2 (https://www.mozilla.org/en-US/MPL/2.0/).
+
+# Main file
+
 import discord, logging
 from discord.ext import commands, tasks
 
